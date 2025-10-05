491a02270c2069afe0de561402e507feb6111534
Ana Sanchez <anisan1995@gmail.com>
2025-10-04 20:13:04 -0400

    Added lab tasks

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,18 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+    import { createSlice } from "@reduxjs/toolkit";
+    import projectors from '../assets/images/projector.jpg';
+    import speaker from '../assets/images/speakers.jpg';
+    import signs from '../assets/images/signs.png';
+    import whiteboard from '../assets/images/whiteboard.webp';
+    import microphone from '../assets/images/microphone.jpg';
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+        {
+        img: projectors,
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: speaker,
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: microphone,
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: whiteboard,
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: signs,
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
--- src/venueSlice.js
+++ src/venueSlice.js
@@ -1,35 +1,40 @@
 // venueSlice.js
 import { createSlice } from "@reduxjs/toolkit";
+import conference from '../assets/images/conferenceroom.jpg';
+import auditorium from '../assets/images/auditorium.png';
+import presentation from '../assets/images/presentationroom.png';
+import smallmeeting from '../assets/images/smallmeetingroom.png';
+import meetingroom from '../assets/images/meetingroom.png';
 
 export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: conference,
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
+      img: auditorium,
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
+      img: presentation,
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: meetingroom,
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
+      img: smallmeeting,
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
@@ -56,5 +61,4 @@ export const venueSlice = createSlice({
 });
 
 export const { incrementQuantity, decrementQuantity } = venueSlice.actions;
-
 export default venueSlice.reducer;
